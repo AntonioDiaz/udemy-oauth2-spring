5e000585265bc1952a67b8c78a1e528475a445e7
Antonio Diaz Arroyo <antonio.diaz.arroyo@gmail.com>
2025-05-01 20:14:21 +0200

    adding scope filter validation.

--- src/main/java/com/adiaz/testing_security/WebSecurity.java
+++ src/main/java/com/adiaz/testing_security/WebSecurity.java
@@ -1,17 +1,23 @@
 package com.adiaz.testing_security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
 
 @EnableWebSecurity
+@Component
 public class WebSecurity {
 
   @Bean
   SecurityFilterChain configure(HttpSecurity http) throws Exception {
     http
-        .authorizeHttpRequests(oauthz -> oauthz.anyRequest().authenticated())
+        .authorizeHttpRequests(oauthz -> oauthz
+            .requestMatchers(HttpMethod.GET, "/users/status")
+              .hasAnyAuthority("SCOPE_email")
+            .anyRequest().authenticated())
         .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwtConfigurer -> {}));
     return http.build();
   }
